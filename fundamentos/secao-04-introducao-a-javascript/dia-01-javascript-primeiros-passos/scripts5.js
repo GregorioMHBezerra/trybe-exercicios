a6760494e180fe8f6a6608fcb3beae433a012fe9
Gregório de M H Bezerra <gregoriodmhbezerra@gmail.com>
2022-10-20 15:22:39 -0300

    Adiciona exercicios fundamentos/4.1

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/scripts5.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/scripts5.js
@@ -4,3 +4,23 @@
 // Caso 'lista', imprima “Você está na nossa lista de espera”.
 // Caso 'reprovada', imprima “Você foi reprovada(o)”.
 // Caso default, imprima a mensagem de “Informação incorreta”.
+
+let statusPessoa = "aprovada"
+
+switch (statusPessoa) {
+  case 'aprovada':
+    console.log('Parabéns, você foi aprovada(o)!')
+    break;
+  
+  case 'lista':
+    console.log('Você está na nossa lista de espera')
+    break;
+  
+  case 'reprovada':
+    console.log('Você foi reprovada(o)')
+    break;
+
+  default:
+    console.log('Informação incorreta')
+    break;
+}
