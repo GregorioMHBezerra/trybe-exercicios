9f03a7aab4a04f8f4f69a331faac3160fa2f6873
Gregório de M H Bezerra <gregoriodmhbezerra@gmail.com>
2022-10-20 14:43:02 -0300

    Adiciona exercicios de fundamentos/4.1

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/scripts3.js
@@ -0,0 +1,10 @@
+const nota = 60
+
+if (nota >= 80) {
+  console.log('Parabens, você foi aprovada(o)')
+} else if (nota < 60){
+  console.log('Você foi reprovada(o)')
+} else {
+  console.log('Você está na nossa lista de espera')
+}
+
